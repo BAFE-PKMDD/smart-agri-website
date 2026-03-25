77712d011b9de728e0e3e25286582c853096af2d
Ronnie Chy <ronnie.chy26@gmail.com>
2026-03-25 14:57:19 +0800

    v 1.3

--- components/header.js
+++ components/header.js
@@ -127,7 +127,7 @@ export default function Header() {
             BAFE Smart Agriculture
             <span>Instrumentation &amp; Electronics</span>
           </div>
-          <span className="header__version">v1.2</span>
+          <span className="header__version">v1.3</span>
         </a>
 
         <nav className={`nav ${menuOpen ? "nav--open" : ""}`} id="nav-menu">
